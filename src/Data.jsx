a6184281b7134d7038397ebbcb30d8d6fd81d425
Francisco Perez <fperezfrancisco4@gmail.com>
2024-10-08 12:17:54 -0700

    images in data link changed

--- src/Data.jsx
+++ src/Data.jsx
@@ -5,9 +5,9 @@ const data = [
   {
     id: 1,
     projectTitle: "Disney Plus Clone",
-    primaryImg: "../public/disney-clone-wide.png",
-    wideImg: "../public/disney-clone-wide.png",
-    squareImg: "../public/disney-square.png",
+    primaryImg: "/disney-clone-wide.png",
+    wideImg: "./disney-clone-wide.png",
+    squareImg: "./disney-square.png",
     detailsPara: `This project is a fully responsive web application replicating the design and functionality of the official Disney Plus platform. Built using React.js and Tailwind CSS, it dynamically fetches and displays movie and TV show selections using The Movie Database (TMDb) API. Users can browse a vast array of titles, select a movie to view detailed information, and discover 20 recommended and 10 similar movies based on their choices. The app is structured to provide a seamless user experience, from smooth navigation to visually appealing design, with all code managed through GitHub.`,
     tools: ["React", "Tailwind Css", "The Movie Database API", "Github"],
     liveURL: "https://fperezfrancisco.github.io/disney-plus-clone/",
@@ -16,9 +16,9 @@ const data = [
   {
     id: 2,
     projectTitle: "Library Ecommerce",
-    primaryImg: "../public/library-screenshot.png",
-    wideImg: "../public/library-screenshot.png",
-    squareImg: "../public/library-long.png",
+    primaryImg: "/library-screenshot.png",
+    wideImg: "./library-screenshot.png",
+    squareImg: "./library-long.png",
     detailsPara:
       "This project is a modern, responsive eCommerce website designed for a seamless user experience. The landing page features a clean, contemporary layout that highlights book selections. Users can browse through available books, view detailed information for each title, and add items to their cart with full management functionality—allowing them to edit quantities, delete items, and track selections easily. Additionally, users can sort books by rating or price for a more tailored browsing experience. Built with a focus on intuitive navigation and efficient user interactions, this project showcases my ability to design and develop functional, user-centric eCommerce applications.",
     tools: ["React", "CSS", "Vite", "Github"],
@@ -28,9 +28,9 @@ const data = [
   {
     id: 3,
     projectTitle: "Movie Search API",
-    primaryImg: "../public/movie-search-wide.png",
-    wideImg: "../public/movie-search-wide.png",
-    squareImg: "../public/movie-search-preview.png",
+    primaryImg: "/movie-search-wide.png",
+    wideImg: "./movie-search-wide.png",
+    squareImg: "./movie-search-preview.png",
     detailsPara:
       "This project is a responsive movie search website built using React.js, Tailwind CSS, and GitHub for version control. It leverages a movie API to allow users to search for films using keywords, with a dynamic loading state that includes a UI skeleton and loading bar to ensure a smooth experience. Upon search, the site displays the top 8 movies based on the user's input and allows users to view detailed information such as synopsis, cast, and release date. Designed for seamless performance and user-friendly navigation, this project demonstrates my skills in API integration, dynamic UI components, and responsive front-end development using modern technologies.",
     tools: ["React", "Tailwind CSS", "Github", "Movie API"],
@@ -40,9 +40,9 @@ const data = [
   {
     id: 4,
     projectTitle: "Trilogy Soccer Website",
-    primaryImg: "../public/trilogy-wide.png",
-    wideImg: "../public/trilogy-wide.png",
-    squareImg: "../public/trilogy-square.png",
+    primaryImg: "/trilogy-wide.png",
+    wideImg: "./trilogy-wide.png",
+    squareImg: "./trilogy-square.png",
     detailsPara:
       "This multi-page website was designed and developed to serve real-world users, enabling them to easily view and purchase soccer training bundles, camps, and private sessions. With a focus on user experience and seamless navigation, the website has significantly enhanced the business’s ability to attract more clients, convert leads, and increase revenue. By streamlining the purchasing process and providing an intuitive layout, the site has successfully funneled users into actionable decisions, helping to grow the business and expand its reach in the soccer training community.",
     tools: ["HTML", "CSS", "Javascript", "Github", "Netlify", "Google Sheets"],
@@ -52,9 +52,9 @@ const data = [
   {
     id: 5,
     projectTitle: "Wordle Clone",
-    primaryImg: "../public/wordle-wide.png",
-    wideImg: "../public/wordle-wide.png",
-    squareImg: "../public/wordle-long.png",
+    primaryImg: "/wordle-wide.png",
+    wideImg: "./wordle-wide.png",
+    squareImg: "./wordle-long.png",
     detailsPara:
       "This project is a fully functional clone of the viral Wordle game, where users attempt to guess a 5-letter word within 6 tries. Built using HTML, CSS, and JavaScript, the web app pulls random words from a dictionary API to generate each new puzzle. It also features local storage integration to track important gameplay statistics, such as the number of wins, total games played, and ongoing streaks. The app delivers an engaging user experience by maintaining smooth interactions and dynamic updates, while offering players an addictive and competitive environment similar to the original game.",
     tools: ["HTML", "Css", "Javascript", "Dictionary API", "Github"],
@@ -64,9 +64,9 @@ const data = [
   {
     id: 6,
     projectTitle: "Superior Plumbing Website",
-    primaryImg: "../public/plumbing-wide.png",
-    wideImg: "../public/plumbing-wide.png",
-    squareImg: "../public/plumbing-square.png",
+    primaryImg: "/plumbing-wide.png",
+    wideImg: "./plumbing-wide.png",
+    squareImg: "./plumbing-square.png",
     detailsPara:
       "This is a fully responsive landing page for the Superior Plumbing company showcasing many important sections from their services to about the company. Built using React Js and tailwind CSS, the website follows HTML best practices as well as optimized SEO practices to ensure best user experience.",
     tools: ["React", "Tailwind CSS", "Github"],
